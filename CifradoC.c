4eeaf42c6faf6aa5b50679fef9f4e1b376c9e3fc
dguizado <angigiacolemar@hotmail.com>
2017-05-25 14:05:56 -0500

    Funcionamiento completo del progrma

--- CifradoC.c
+++ CifradoC.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include <string.h>
-void main(){
+
+int main(){
 
   int key;
   char mensaje[40];
 
   printf("Cifrado Ciclico\n");
   printf("Ingrese mensaje a cifrar: \n");
-  scanf("%s" ,&mensaje);
+  scanf("%s" ,mensaje);
   printf("Ingrese la llave numerica: \n");
-  scanf("%d" ,&key);
-  
-  char cifrado(int key, char mensaje[]){
+  scanf("%d",&key);
+  cifrado(key,mensaje);
+
+	return;
+}
+
+void cifrado(int key, char mensaje[]){
     int i;
-    char message;
     for(i=0;i<strlen(mensaje);i++){
-      if((key>0) && ((mensaje[i]>=65 && mensaje[i]<90) || (mensaje[i]>=97 && mensaje[i]<122))){
-         message=mensaje[i]+key;
-         printf("El mensaje cifrado es: ", message); 
-       }
-      
-      else if((key<0) && ((mensaje[i]>=65 && mensaje[i]<90) || (mensaje[i]>=97 && mensaje[i]<122))){
-          message=mensaje[i]-key;
-          printf("El mensaje cifrado es: ", message);
+       if((mensaje[i]>=65 && mensaje[i]<90) || (mensaje[i]>=97 && mensaje[i]<122)){
+         printf("%c",mensaje[i]+key);
        }
     }
-   
-  }
+
 }
+
