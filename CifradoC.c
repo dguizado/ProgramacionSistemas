23e63a9f373b8a48114699ecc18ceff57f068d8d
dguizado <angigiacolemar@hotmail.com>
2017-05-25 08:46:31 -0500

    Mejoras en el codigo y optimizacion de este

--- CifradoC.c
+++ CifradoC.c
@@ -1,37 +1,30 @@
 #include <stdio.h>
 #include <string.h>
+void main(){
 
-main()
-{
-  int key=0;
-  int i=0;
+  int key;
   char mensaje[40];
 
   printf("Cifrado Ciclico\n");
   printf("Ingrese mensaje a cifrar: \n");
-  scanf("%s" ,mensaje);
+  scanf("%s" ,&mensaje);
   printf("Ingrese la llave numerica: \n");
-  scanf("%i" ,key);
+  scanf("%d" ,&key);
   
   char cifrado(int key, char mensaje[]){
-  	for (i=0; i < strlen(mensaje); i++){
-  	   if(mensaje [i] >= 'a' && mensaje [i] <= 'z'){
-             if (mensaje [i] +key > 'z'){
-                return mensaje [i] = 'a' - 'z' + mensaje [i] + key - 1;
-             } else if (mensaje [i] + key < 'a'){
-                return mensaje [i] = 'z' - 'a' + mensaje [i] + key + 1;
-             } else {
-                return mensaje [i] += key;
-             }
-           } else if (mensaje [i] >= 'A' && mensaje [i] <= 'Z') {
-              if (mensaje [i] + key > 'Z') {
-                 return mensaje [i] = 'A' - 'Z' + mensaje [i] + key - 1;
-              } else if (mensaje [i] + key < 'A') {
-                 return mensaje [i] = 'Z' - 'A' + mensaje [i] + key + 1;
-              } else {
-                 return mensaje [i] += key;
-              }
-        }
-  }       
-}            
+    int i;
+    char message;
+    for(i=0;i<strlen(mensaje);i++){
+      if((key>0) && ((mensaje[i]>=65 && mensaje[i]<90) || (mensaje[i]>=97 && mensaje[i]<122))){
+         message=mensaje[i]+key;
+         printf("El mensaje cifrado es: ", message); 
+       }
+      
+      else if((key<0) && ((mensaje[i]>=65 && mensaje[i]<90) || (mensaje[i]>=97 && mensaje[i]<122))){
+          message=mensaje[i]-key;
+          printf("El mensaje cifrado es: ", message);
+       }
+    }
+   
+  }
 }
